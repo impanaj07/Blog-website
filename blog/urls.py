ec8c7700bfc7c7a4ab2871a03bae776803f2f21c
impanajgowda14@gmail.com <154488357+impanaj07@users.noreply.github.com>
2024-09-17 17:16:53 +0530

    Added index.html base.html index.css and app.css

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
-from django.url import path
+from django.urls import path
 from . import views
 
-urlspattern=[
+urlpatterns =[
     path("",views.starting_page,name="starting-page"),
     path("posts",views.posts,name="posts-page"),
     path("posts/<slug:slug>",views.post_detail,
--- my_site/urls.py
+++ my_site/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include("blog.urls"))#this will take u to the blog apps url where all the pages are there 
+    path("",include("blog.urls")),#this will take u to the blog apps url where all the pages are there 
 ]
