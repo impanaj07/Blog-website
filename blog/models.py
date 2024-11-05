bc716f33a03e691fe328f18e1ce9b59e497d6052
impanajgowda14@gmail.com <154488357+impanaj07@users.noreply.github.com>
2024-11-05 19:59:30 +0530

    updated views by adding from db ,added filters to the admin page and updated models.py

--- blog/models.py
+++ blog/models.py
@@ -2,12 +2,19 @@ from django.db import models
 from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
 class Tag(models.Model):
     caption=models.CharField(max_length=20)
+    def __str__(self):
+        return self.caption
 class Author(models.Model):
     first_name=models.CharField(max_length=100)
     last_name=models.CharField(max_length=100)
     email_address=models.EmailField()
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    def __str__(self):
+        return self.full_name()
 class Post(models.Model):
     title =models.CharField(max_length=150)
     excerpt=models.CharField(max_length=200)
